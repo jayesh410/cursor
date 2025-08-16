c6bb1ba3a20a5ac98240caecbd8d2191516ed860
Jayesh Pandhare <144468239+jayesh410@users.noreply.github.com>
2025-08-16 08:49:05 +0000

    responsive.orbit,

---
+++ threed/src/main.js
@@ -0,0 +1,37 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 , wireframe: true} );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+const canvas = document.querySelector('canvas')
+const renderer = new THREE.WebGLRenderer({canvas});
+renderer.setSize( window.innerWidth, window.innerHeight);
+
+window.addEventListener('resize',()=>{
+   renderer.setSize( window.innerWidth, window.innerHeight);
+   camera.aspect = window.innerWidth / window.innerHeight;
+   camera.updateProjectionMatrix();    
+}
+)
+
+const controls = new OrbitControls( camera, renderer.domElement );
+
+function animate() {
+window.requestAnimationFrame(animate);
+  renderer.render( scene, camera );
+
+
+  cube.rotation.x += 0.01; // small number = slow
+  cube.rotation.y += 0; // bigger number = faster
+
+controls.update();
+}
+animate();
